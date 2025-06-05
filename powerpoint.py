472af37081690a852a61af856c55f8d19ebf2a45
Alastair <62060392+alastairherd@users.noreply.github.com>
2025-06-05 17:12:57 +0100

    Implement cleanup and security improvements

--- powerpoint.py
+++ powerpoint.py
@@ -1,22 +1,19 @@
-from slide_making import *
+import slide_making as sm
 from pptx import Presentation
 
-template_file = 'template.pptx'
 
-# Open the template file
-# Create new class instance (prs)
-prs = Presentation(template_file)
+def main():
+    template_file = 'template.pptx'
+    prs = Presentation(template_file)
+    for i in range(1, 23):
+        try:
+            print(i)
+            sm.slide_writer(i, prs)
+        except Exception:
+            print(f"Error on slide {i}")
 
-
-## Currently trying to sort out adding catechism pages
-for i in range (1,23):
-    try:
-        print(i)
-        slide_writer(i, prs)
-    except:
-        print(f"Error on slide {i}")
+    prs.save('example_from_template.pptx')
 
 
-
-# Save the PowerPoint file
-prs.save('example_from_template.pptx')
+if __name__ == "__main__":
+    main()
